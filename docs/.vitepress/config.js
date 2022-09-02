d8979d12e605198e36b39866530e3fb8db8867a9
linjuncheng <linjuncheng@bytedance.com>
2022-09-03 01:22:34 +0800

    feat: open web

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -95,6 +95,7 @@ module.exports = {
             { link: "/pages/network/overview.md", text: "概述" },
             { link: "/pages/network/physics.md", text: "物理层" },
             { link: "/pages/network/datalink.md", text: "数据链路层" },
+            { link: "/pages/network/web.md", text: "网络层" },
           ],
         },
       ],
