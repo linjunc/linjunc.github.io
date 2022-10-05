329f2412af20374d344388141f3c32e42f01ec1f
linjunc <ljc15113180751@163.com>
2022-10-05 10:45:14 +0800

    feat: array finish

--- code/handwriting/array.js
+++ code/handwriting/array.js
@@ -44,7 +44,7 @@ function flatten(arr) {
 // }
 
 function flat(arr, depth) {
-    if (!arr || depth < 0) return arr;
+    if (!arr || depth < 0) return arr
     return arr.reduce((prev, next) => {
         return prev.concat(Array.isArray(next) ? flat(next, depth - 1) : next)
     }, [])
@@ -52,7 +52,7 @@ function flat(arr, depth) {
 
 const arr = [1, [2, [3, [4, 5, [6]], 5]]]
 
-console.log(flat(arr));
+console.log(flat(arr))
 
 
 // 数组去重
@@ -62,24 +62,24 @@ function distinct(arr) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] === arr[i]) {
                 arr.splice(j, 1)
-                j--; // 数组长度变了
+                j-- // 数组长度变了
             }
         }
     }
-    return arr;
+    return arr
 }
 
 // filter 和 indexOf
 function distinct(a, b) {
     const arr = a.concat(b)
     return arr.filter((item, index) => {
-        return arr.indexOf(item) === index;
+        return arr.indexOf(item) === index
     })
 }
 
 // Set
 const array = [1, 2, 2, 3, 1]
-const res = [...new Set(array)];
+const res = [...new Set(array)]
 
 // Map
 function uniqueArray(arr) {
@@ -132,7 +132,7 @@ function unique(arr) {
 
 Array.prototype.push = function () {
     for (let i = 0; i < arguments.length; i++) {
-        this[this.length] = arguments[i];
+        this[this.length] = arguments[i]
     }
     return this.length // 返回长度
 }
@@ -168,4 +168,143 @@ const fun = () => {
         }
     }
     printf('')
+}
+
+// reduce
+
+Array.prototype.myReduce = function (fn, initVal) {
+    let result = initVal, i = 0
+    if (typeof initVal === 'undefined') {
+        result = this[i]
+        i++
+    }
+    // this 是一个数组
+    while (i < this.length) {
+        result = fn(result, this[i])
+    }
+    return result
+}
+
+// splice
+const sliceDeleteElements = (array, startIndex, deleteCount, deleteArr) => {
+    for (let i = 0; i < deleteCount; i++) {
+        const index = startIndex + i
+        if (index in array) {
+            let current = array[index]
+            deleteArr[i] = current
+        }
+    }
+}
+
+const movePostElements = (array, startIndex, len, deleteCount, addElements) => {
+    if (deleteCount === addElements.length) {
+        return
+    } else if (deleteCount > addElements.length) {
+        // 删除的元素比新增的元素多，那么后面的元素整体向前挪动
+        // 一共需要挪动 len - startIndex - deleteCount 个元素
+        for (let i = startIndex + deleteCount; i < len; i++) {
+            let fromIndex = i
+            let toIndex = i - (deleteCount - addElements.length) // 目标位置
+            if (fromIndex in array) {
+                array[toIndex] = array[fromIndex]
+            } else {
+                delete array[toIndex]
+            }
+        }
+        // 注意注意！这里我们把后面的元素向前挪，相当于数组长度减小了，需要删除冗余元素
+        // 目前长度为 len + addElements - deleteCount
+        for (let i = len - 1; i >= len + addElements.length - deleteCount; i--) {
+            delete array[i]
+        }
+    } else if (deleteCount < addElements.length) {
+        // 删除的元素比新增的元素少，那么后面的元素整体向后挪动
+        for (let i = len - 1; i >= startIndex + deleteCount; i--) {
+            let fromIndex = i
+            let toIndex = i + (addElements.length - deleteCount)
+            if (fromIndex in array) {
+                array[toIndex] = array[fromIndex]
+            } else {
+                delete array[toIndex]
+            }
+        }
+    }
+}
+
+
+const computeStartIndex = (startIndex, len) => {
+    // 处理索引负数的情况
+    if (startIndex < 0) {
+        return startIndex + len > 0 ? startIndex + len : 0
+    }
+    return startIndex >= len ? len : startIndex
+}
+
+const computeDeleteCount = (startIndex, len, deleteCount, argumentsLen) => {
+    // 删除数目没有传，默认删除startIndex及后面所有的
+    if (argumentsLen === 1)
+        return len - startIndex
+    // 删除数目过小
+    if (deleteCount < 0)
+        return 0
+    // 删除数目过大
+    if (deleteCount > len - deleteCount)
+        return len - startIndex
+    return deleteCount
+}
+
+
+Array.prototype._splice = function (start, deleteCount, ...addElements) {
+    const argumentsLen = arguments.length
+    const array = Object(this)
+    const len = array.length
+    const deleteArr = new Array(deleteCount) // 删除的数组，也就是要返回的东西
+
+    startIndex = computeStartIndex(start, len)
+    deleteCount = computeDeleteCount(startIndex, len, deleteCount, argumentsLen)
+
+
+    // 判断 sealed 对象和 frozen 对象, 即 密封对象 和 冻结对象
+    if (Object.isSealed(array) && deleteCount !== addElements.length) {
+        throw new TypeError('the object is a sealed Object')
+    } else if (Object.isFrozen(array) && (deleteCount > 0 || addElements.length > 0)) {
+        throw new TypeError('the object is frozen object')
+    }
+
+    // 拷贝删除的元素
+    sliceDeleteElements(array, startIndex, deleteCount, deleteArr)
+    // 移动删除元素后面的元素
+    movePostElements(array, startIndex, len, deleteCount, addElements)
+    // 插入新元素
+    for (let i = 0; i < addElements.length; i++) {
+        array[startIndex + i] = addElements[i]
+    }
+
+    array.length = len - deleteCount + addElements.length
+
+    return deleteArr
+}
+
+// String
+
+function sIndexOf(str, searchStr, fromIndex = 0) {
+    const regex = new RegExp(`${searchStr}`, 'ig');
+    regex.lastIndex = fromIndex
+    const result = regex.exec(str)
+    return result ? result.index : -1
+}
+
+function aIndexOf(arr, elem, fromIndex = 0) {
+    if(!elem) return -1
+    for(let i = fromIndex; i < arr.length; i++) {
+        if(arr[i] === elem) return i
+    }
+    return -1
+}
+
+function indexOf(items, elem, fromIndex = 0) {
+    const isArray = Array.isArray(items)
+    const isString = Object.prototype.toString.call(items) === '[object String]'
+    if(!isArray && !isString) throw new SyntaxError()
+    if(isArray) return aIndexOf(items, elem, fromIndex)
+    return sIndexOf(items, elem, fromIndex) 
 }
