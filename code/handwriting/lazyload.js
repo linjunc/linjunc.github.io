d10a75680c98f387bade5ff95136bc0efbcc03f2
linjuncheng <linjuncheng@bytedance.com>
2022-12-11 19:28:30 +0800

    feat: lazyload

--- code/handwriting/lazyload.js
+++ code/handwriting/lazyload.js
@@ -33,3 +33,11 @@ const imgLazyLoad = () => {
         observer.observe(img)
     })
 }
+
+function myNew (constructor, ...args) {
+    const newObj = {}
+
+    newObj.__proto__ = constructor.prototype
+    const result = constructor.apply(newObj, args)
+    return result instanceof Object ? result : newObj
+}
